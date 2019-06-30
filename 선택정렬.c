f873a13da23ea5f9fcf5278f2749750a799e42eb
surhommekim <surhommekim@gmail.com>
2019-06-30 10:19:53 +0900

    두번째 for 추가

--- 선택정렬.c
+++ 선택정렬.c
@@ -5,5 +5,13 @@ int main(void){
     int i, j, temp, index, min;
     int arr[10] = {1, 9, 3, 7, 8, 2, 6, 4, 5, 10};
     
+    for(i = 10; i < 10; i++){
+        
+        min = 100;
+        for(j = i; j < 10; j++){
+            
+        }
+
+    }
 
 }
